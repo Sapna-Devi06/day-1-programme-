2dcae4fc649c4565ec75743f9be551e2ca1e4683
Sapna-Devi06 <nd747654@gmail.com>
2026-06-24 23:37:45 +0530

    Add files via upload

---
+++ minarr.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout<<"Enter size of array: ";
+    cin>>n;
+
+    int arr[n];
+
+    cout<<"Enter array elements: ";
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    int min = arr[0];
+
+    for(int i=1; i<n; i++)
+    {
+        if(arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+
+    cout<<"Minimum element = "<<min;
+
+    return 0;
+}
---
+++ sumarr.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, sum = 0;
+
+    cout<<"Enter size of array: ";
+    cin>>n;
+
+    int arr[n];
+
+    cout<<"Enter array elements: ";
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        sum = sum + arr[i];
+    }
+
+    cout<<"Sum of array = "<<sum;
+
+    return 0;
+}
